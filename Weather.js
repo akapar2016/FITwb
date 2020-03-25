f45e82eb65067745966acbb992ea90425c13be43
Aayush Kapar <aayushkapar@gmail.com>
2020-03-24 22:12:36 -0400

    Weather

--- Weather.js
+++ Weather.js
@@ -1,3 +1,18 @@
-function getDepWeather(icao) {
 
+function getDepWeather(icao) {
+    var url = 'https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=' + icao + '&hoursBeforeNow=1'
+    var xhttp = new XMLHttpRequest();
+    var xmlParser = new DOMParser();
+    var xmlData;
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            xmlData = xmlParser.parseFromString(this.responseText);
+        }
+    }
+
+    xhttp.open("GET", url, true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send();
+
+    document.getElementById("DWeatherTime").innerHTML = xmlData;
 }
