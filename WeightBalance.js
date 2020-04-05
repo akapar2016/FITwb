6021e6aecc4503a47c4ba445760629aeebd18c57
Aayush Kapar <aayushkapar@gmail.com>
2020-04-05 16:38:05 -0400

    WB complete

--- WeightBalance.js
+++ WeightBalance.js
@@ -80,21 +80,42 @@ function calculateBaggage(input) {
     calculateTotals();
 }
 
-function calculateFuel(input) {
-    document.getElementById("MFuel").innerHTML = 
+function calculateFuel(parentId, input) {
+    if (parentId.charAt(0) == "W") {
+        document.getElementById("MFuel").innerHTML = 
             (document.getElementById("AFuel").innerText * input).toFixed(1);
+        document.getElementById("FuelQuantity").getElementsByTagName("input")[0].value = (input/6).toFixed(2);
+    } else {
+        document.getElementById("WFuel").getElementsByTagName("input")[0].value = input*6;
+        document.getElementById("MFuel").innerHTML = 
+            (document.getElementById("AFuel").innerText * (input * 6)).toFixed(1);
+    }
     calculateTotals();
 }
 
-function calculateFuelTaxi(input) {
-    document.getElementById("MFuelTaxi").innerHTML = 
+function calculateFuelTaxi(parentId, input) {
+    if (parentId.charAt(0) == "W") {
+        document.getElementById("MFuelTaxi").innerHTML = 
             (document.getElementById("AFuelTaxi").innerText * input).toFixed(1);
+        document.getElementById("FuelQuantityTaxi").getElementsByTagName("input")[0].value = (input/6).toFixed(2);
+    } else {
+        document.getElementById("WFuelTaxi").getElementsByTagName("input")[0].value = input*6;
+        document.getElementById("MFuelTaxi").innerHTML = 
+            (document.getElementById("AFuelTaxi").innerText * (input * 6)).toFixed(1);
+    }
     calculateTotals();
 }
 
-function calculateFuelTrip(input) {
-    document.getElementById("MFuelTrip").innerHTML = 
+function calculateFuelTrip(parentId, input) {
+    if (parentId.charAt(0) == "W") {
+        document.getElementById("MFuelTrip").innerHTML = 
             (document.getElementById("AFuelTrip").innerText * input).toFixed(1);
+        document.getElementById("FuelQuantityTrip").getElementsByTagName("input")[0].value = (input/6).toFixed(2);
+    } else {
+        document.getElementById("WFuelTrip").getElementsByTagName("input")[0].value = input*6;
+        document.getElementById("MFuelTrip").innerHTML = 
+            (document.getElementById("AFuelTrip").innerText * (input * 6)).toFixed(1);
+    }
     calculateTotals();
 }
 
@@ -105,6 +126,8 @@ function calculateTotals() {
             parseFloat(document.getElementById("WSeat34").getElementsByTagName("input")[0].value) +
             parseFloat(document.getElementById("WCargo").getElementsByTagName("input")[0].value) +
             parseFloat(document.getElementById("WFuel").getElementsByTagName("input")[0].value)).toFixed(1);
+    if (document.getElementById("WRamp").innerHTML == "NaN")
+        document.getElementById("WRamp").innerHTML = "--"; 
 
     document.getElementById("MRamp").innerHTML = 
             (parseFloat(document.getElementById("Moment").innerText) +
@@ -112,32 +135,49 @@ function calculateTotals() {
             parseFloat(document.getElementById("MSeat34").innerText) +
             parseFloat(document.getElementById("MCargo").innerText) +
             parseFloat(document.getElementById("MFuel").innerText)).toFixed(1);
+    if (document.getElementById("MRamp").innerHTML == "NaN")
+        document.getElementById("MRamp").innerHTML = "--"; 
 
     document.getElementById("ARamp").innerHTML = 
             (parseFloat(document.getElementById("MRamp").innerHTML) / 
             parseFloat(document.getElementById("WRamp").innerHTML)).toFixed(1);
+    if (document.getElementById("ARamp").innerHTML == "NaN")
+        document.getElementById("ARamp").innerHTML = "--"; 
 
     document.getElementById("WTakeOff").innerHTML = 
             (parseFloat(document.getElementById("WRamp").innerText) -
             parseFloat(document.getElementById("WFuelTaxi").getElementsByTagName("input")[0].value)).toFixed(1);
+    if (document.getElementById("WTakeOff").innerHTML == "NaN")
+        document.getElementById("WTakeOff").innerHTML = "--"; 
 
     document.getElementById("MTakeOff").innerHTML = 
             (parseFloat(document.getElementById("MRamp").innerText) -
             parseFloat(document.getElementById("MFuelTaxi").innerText)).toFixed(1);
+    if (document.getElementById("MTakeOff").innerHTML == "NaN")
+        document.getElementById("MTakeOff").innerHTML = "--"; 
 
     document.getElementById("ATakeOff").innerHTML = 
             (parseFloat(document.getElementById("MTakeOff").innerHTML) / 
             parseFloat(document.getElementById("WTakeOff").innerHTML)).toFixed(1);
-    
+    if (document.getElementById("ATakeOff").innerHTML == "NaN")
+        document.getElementById("ATakeOff").innerHTML = "--"; 
+
     document.getElementById("WLanding").innerHTML = 
             (parseFloat(document.getElementById("WTakeOff").innerText) -
             parseFloat(document.getElementById("WFuelTrip").getElementsByTagName("input")[0].value)).toFixed(1);
+    if (document.getElementById("WLanding").innerHTML == "NaN")
+        document.getElementById("WLanding").innerHTML = "--"; 
 
     document.getElementById("MLanding").innerHTML = 
             (parseFloat(document.getElementById("MTakeOff").innerText) -
             parseFloat(document.getElementById("MFuelTrip").innerText)).toFixed(1);
+    if (document.getElementById("MLanding").innerHTML == "NaN")
+        document.getElementById("MLanding").innerHTML = "--"; 
 
     document.getElementById("ALanding").innerHTML = 
             (parseFloat(document.getElementById("MLanding").innerHTML) / 
             parseFloat(document.getElementById("WLanding").innerHTML)).toFixed(1);
+    if (document.getElementById("ALanding").innerHTML == "NaN")
+        document.getElementById("ALanding").innerHTML = "--"; 
+
 }
