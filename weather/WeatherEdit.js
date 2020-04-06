f077fd5a2feea2437af2e236d50840b554544dba
Aayush Kapar <aayushkapar@gmail.com>
2020-04-05 20:50:47 -0400

    weather refined

--- weather/WeatherEdit.js
+++ weather/WeatherEdit.js
@@ -1,5 +1,6 @@
 
 function editDepWeatherValues(value) {
+    value = value.getElementsByTagName("METAR")[0];
     document.getElementById("DWeatherTime").innerHTML = 
         value.getElementsByTagName("observation_time")[0].childNodes[0].nodeValue;
 
@@ -10,20 +11,30 @@ function editDepWeatherValues(value) {
     document.getElementById("DWeatherVis").innerHTML = 
         value.getElementsByTagName("visibility_statute_mi")[0].childNodes[0].nodeValue;
 
-    document.getElementById("DWeatherSky").innerHTML = 
-        value.getElementsByTagName("observation_time")[0].childNodes[0].nodeValue;
+    document.getElementById("DWeatherSky").innerHTML = "";
+    var skycond = value.getElementsByTagName("sky_condition");
+    for (var i = 0; i < skycond.length; i++) {
+        document.getElementById("DWeatherSky").innerHTML = 
+            document.getElementById("DWeatherSky").innerHTML + 
+            skycond[i].getAttribute('sky_cover') + " " + 
+            skycond[i].getAttribute('cloud_base_ft_agl') + " "
+    }
 
-    document.getElementById("DWeatherTemp").innerHTML = 
+    document.getElementById("DWeatherDisp").innerHTML = 
+        (value.getElementsByTagName("wx_string").length > 0 ? 
+        value.getElementsByTagName("wx_string")[0].childNodes[0].nodeValue : "None");
+
+    document.getElementById("DWeatherTemp").getElementsByTagName("input")[0].value = 
         value.getElementsByTagName("temp_c")[0].childNodes[0].nodeValue;
 
-    document.getElementById("DWeatherDew").innerHTML = 
+    document.getElementById("DWeatherDew").getElementsByTagName("input")[0].value = 
         value.getElementsByTagName("dewpoint_c")[0].childNodes[0].nodeValue;
 
-    document.getElementById("Delevation").innerHTML = 
+    document.getElementById("Delevation").getElementsByTagName("input")[0].value = 
         (parseFloat(value.getElementsByTagName("elevation_m")[0].childNodes[0].nodeValue) * 3.28084).toFixed(0);
 
-    document.getElementById("DWeatherAlt").innerHTML = 
-        value.getElementsByTagName("altim_in_hg")[0].childNodes[0].nodeValue;
+    document.getElementById("DWeatherAlt").getElementsByTagName("input")[0].value = 
+        parseFloat(value.getElementsByTagName("altim_in_hg")[0].childNodes[0].nodeValue).toFixed(2);
     
     document.getElementById("DWeatherRaw").innerHTML = 
         value.getElementsByTagName("raw_text")[0].childNodes[0].nodeValue;
@@ -33,19 +44,18 @@ function editDepWeatherValues(value) {
 
 function weatherDepOnEditChange() {
     document.getElementById("DWeatherTempDif").innerHTML = 
-        (parseFloat(document.getElementById("DWeatherTemp").innerHTML) - 
-        parseFloat(document.getElementById("DWeatherDew").innerHTML)).toFixed(1);
+        (parseFloat(document.getElementById("DWeatherTemp").getElementsByTagName("input")[0].value) - 
+        parseFloat(document.getElementById("DWeatherDew").getElementsByTagName("input")[0].value)).toFixed(1);
 
     document.getElementById("DpressA").innerHTML = 
-        ((29.92 - parseFloat(document.getElementById("DWeatherAlt").innerHTML)) * 1000 + 
-        parseFloat(document.getElementById("Delevation").innerHTML)).toFixed(1);
+        ((29.92 - parseFloat(document.getElementById("DWeatherAlt").getElementsByTagName("input")[0].value)) * 1000 + 
+        parseFloat(document.getElementById("Delevation").getElementsByTagName("input")[0].value)).toFixed(1);
 
-    var stdTemp = 15 - (parseFloat(document.getElementById("Delevation").innerHTML) / 1000) * 2;
+    var stdTemp = 15 - (parseFloat(document.getElementById("Delevation").getElementsByTagName("input")[0].value) / 1000) * 2;
     document.getElementById("DdAlt").innerHTML = 
         (parseFloat(document.getElementById("DpressA").innerHTML) + 
-        (120 * (document.getElementById("DWeatherTemp").innerHTML - 
-        (15 - stdTemp)))).toFixed(1);
-
+        (120 * (document.getElementById("DWeatherTemp").getElementsByTagName("input")[0].value - 
+        stdTemp))).toFixed(1);
 }
 
 function editArWeatherValues(value) {
@@ -59,20 +69,30 @@ function editArWeatherValues(value) {
     document.getElementById("AWeatherVis").innerHTML = 
         value.getElementsByTagName("visibility_statute_mi")[0].childNodes[0].nodeValue;
 
-    document.getElementById("AWeatherSky").innerHTML = 
-        value.getElementsByTagName("observation_time")[0].childNodes[0].nodeValue;
+    document.getElementById("AWeatherSky").innerHTML = "";
+        var skycond = value.getElementsByTagName("sky_condition");
+        for (var i = 0; i < skycond.length; i++) {
+            document.getElementById("AWeatherSky").innerHTML = 
+                document.getElementById("AWeatherSky").innerHTML + 
+                skycond[i].getAttribute('sky_cover') + " " + 
+                skycond[i].getAttribute('cloud_base_ft_agl') + " "
+        }
 
-    document.getElementById("AWeatherTemp").innerHTML = 
+    document.getElementById("AWeatherDisp").innerHTML = 
+        (value.getElementsByTagName("wx_string").length > 0 ? 
+        value.getElementsByTagName("wx_string")[0].childNodes[0].nodeValue : "None");
+
+    document.getElementById("AWeatherTemp").getElementsByTagName("input")[0].value = 
         value.getElementsByTagName("temp_c")[0].childNodes[0].nodeValue;
 
-    document.getElementById("AWeatherDew").innerHTML = 
+    document.getElementById("AWeatherDew").getElementsByTagName("input")[0].value = 
         value.getElementsByTagName("dewpoint_c")[0].childNodes[0].nodeValue;
 
-    document.getElementById("Aelevation").innerHTML = 
+    document.getElementById("Aelevation").getElementsByTagName("input")[0].value = 
         (parseFloat(value.getElementsByTagName("elevation_m")[0].childNodes[0].nodeValue) * 3.28084).toFixed(0);
 
-    document.getElementById("AWeatherAlt").innerHTML = 
-        value.getElementsByTagName("altim_in_hg")[0].childNodes[0].nodeValue;
+    document.getElementById("AWeatherAlt").getElementsByTagName("input")[0].value = 
+        parseFloat(value.getElementsByTagName("altim_in_hg")[0].childNodes[0].nodeValue).toFixed(2);
     
     document.getElementById("AWeatherRaw").innerHTML = 
         value.getElementsByTagName("raw_text")[0].childNodes[0].nodeValue;
@@ -82,17 +102,17 @@ function editArWeatherValues(value) {
 
 function weatherArrOnEditChange() {
     document.getElementById("AWeatherTempDif").innerHTML = 
-        (parseFloat(document.getElementById("AWeatherTemp").innerHTML) - 
-        parseFloat(document.getElementById("AWeatherDew").innerHTML)).toFixed(1);
+        (parseFloat(document.getElementById("AWeatherTemp").getElementsByTagName("input")[0].value) - 
+        parseFloat(document.getElementById("AWeatherDew").getElementsByTagName("input")[0].value)).toFixed(1);
 
     document.getElementById("ApressA").innerHTML = 
-        ((29.92 - parseFloat(document.getElementById("AWeatherAlt").innerHTML)) * 1000 + 
-        parseFloat(document.getElementById("Aelevation").innerHTML)).toFixed(1);
+        ((29.92 - parseFloat(document.getElementById("AWeatherAlt").getElementsByTagName("input")[0].value)) * 1000 + 
+        parseFloat(document.getElementById("Aelevation").getElementsByTagName("input")[0].value)).toFixed(1);
 
-    var stdTemp = 15 - (parseFloat(document.getElementById("Aelevation").innerHTML) / 1000) * 2;
+    var stdTemp = 15 - (parseFloat(document.getElementById("Aelevation").getElementsByTagName("input")[0].value) / 1000) * 2;
     document.getElementById("AdAlt").innerHTML = 
         (parseFloat(document.getElementById("ApressA").innerHTML) + 
-        (120 * (document.getElementById("AWeatherTemp").innerHTML - 
-        (15 - stdTemp)))).toFixed(1);
+        (120 * (document.getElementById("AWeatherTemp").getElementsByTagName("input")[0].value - 
+        stdTemp))).toFixed(1);
 
 }
